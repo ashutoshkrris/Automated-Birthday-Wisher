0d0c473e8b0c70bdb33972fb334e60bb2facf1e6
Ashutosh Krishna <47353498+ashutoshkrris@users.noreply.github.com>
2020-07-01 11:42:42 +0530

    added SMS sending facility
    Now SMS will also be sent using Fast2SMS service.
    You just need to register on www.fast2sms.com and find your API KEY in the DEV API section on the site.

--- wisher.py
+++ wisher.py
@@ -2,6 +2,7 @@ import pandas as pd
 import datetime
 import smtplib
 import time
+import requests
 from win10toast import ToastNotifier
 
 #your gmail credentials here
@@ -22,6 +23,23 @@ def sendEmail(to,sub,msg):
 
     while toast.notification_active():
         time.sleep(0.1)
+        
+ def sendsms(to,msg,name,sub):
+    url = "https://www.fast2sms.com/dev/bulk"
+    payload = f"sender_id=FSTSMS&message={msg}&language=english&route=p&numbers={to}"
+    headers = {
+        'authorization': "API_KEY_HERE",
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Cache-Control': "no-cache",
+        }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response.text)
+    print(f"SMS sent to {to} with subject : {sub} and message : {msg}")
+    toast.show_toast("SMS Sent!" , f"{name} was sent message", threaded=True, icon_path=None, duration=6)
+
+    while toast.notification_active():
+        time.sleep(0.1)
 
 if __name__=="__main__":
     df = pd.read_excel("excelsheet.xlsx")                           #read the excel sheet having all the details
@@ -34,6 +52,7 @@ if __name__=="__main__":
         bday = item['Birthday'].strftime("%d-%m")                   #stripping the birthday in excel sheet as : DD-MM
         if (today==bday) and yearNow not in str(item['Year']):      #condition checking
             sendEmail(item['Email'], "Happy Birthday", msg)         #calling the sendEmail function
+            sendsms(item['Contact'], msg, item['NAME'], "Happy Birthday")           #calling the sendsms function
             writeInd.append(index)                                  
 
     for i in writeInd:
